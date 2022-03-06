f72e65adfe5f1db5e085c2ecea96ebb669780a80
qwEGAr <alexxo@inbox.ru>
2022-03-06 20:16:19 +0300

    add save/load levels mode

--- constants.js
+++ constants.js
@@ -1,24 +1,24 @@
 const scoreOut = document.querySelector("#score"),
   gameOverScreen = document.querySelector(".game-over-screen"),
   gameOverScore = document.querySelector(".score-in-game-over-screen"),
-  startGameScreen = document.querySelector(".start-game-screen")
+  startGameScreen = document.querySelector(".start-game-screen");
 
 const area = document.querySelector("#area"),
   genBtn = document.querySelector("#gen"),
   saveLevelBtn = document.querySelector("#save-level-btn"),
-  loadLevelBtn = document.querySelector("#load-level-btn"),
+  deleteSelectedLevelsBtn = document.querySelector("#delete-selected-levels-btn"),
+  deleteAllLevelsBtn = document.querySelector("#delete-all-levels-btn"),
   rstBtn = document.querySelector("#reset"),
   out = document.querySelector("#out"),
   impTeleport = document.querySelector("#teleport"),
   bigWorldCheck = document.querySelector("#big-world"),
   boxes = area.querySelectorAll(".box"),
-  saveLevelsOut = document.querySelector("#save-levels"),
-  saveLevelScreen = document.querySelector(".save-level-screen")
+  saveLevelScreen = document.querySelector(".save-level-screen");
 
+const colorsPanel = document.querySelector("#colors");
+const colorsPanelBtn = document.querySelector("#color-panel-btn");
 
-const colorsPanel = document.querySelector("#colors")
-const colorsPanelBtn = document.querySelector("#color-panel-btn")
-
+let saveLevelsOut = document.querySelector("#save-levels-out");
 
 const cnv = document.querySelector("#cnv");
 const ctx = cnv.getContext("2d");
@@ -30,10 +30,10 @@ let CELL = 20;
 
 const SNAKE = {
   body: [
-    { x: 7,y: 3,head: true },
-    { x: 6,y: 3,head: false },
-    { x: 5,y: 3,head: false },
-    { x: 4,y: 3,head: false },
+    { x: 7, y: 3, head: true },
+    { x: 6, y: 3, head: false },
+    { x: 5, y: 3, head: false },
+    { x: 4, y: 3, head: false },
   ],
   go: "stop",
 };
@@ -44,101 +44,99 @@ const COLORS = {
   snakeHead: "#ea4c89",
   apple: "green",
   wall: "grey",
-  teleport: "blue"
-}
+  teleport: "blue",
+};
 
 const MAPS = {
   level1: [
-    { x: 29,y: 7,status: "teleport" },
-    { x: 4,y: 15,status: "teleport" },
-    { x: 20,y: 4 },
-    { x: 21,y: 4 },
-    { x: 22,y: 4 },
-    { x: 23,y: 4 },
-    { x: 24,y: 4 },
-    { x: 25,y: 4 },
-    { x: 26,y: 4 },
-    { x: 27,y: 4 },
-    { x: 28,y: 4 },
-    { x: 29,y: 4 },
-    { x: 30,y: 4 },
-    { x: 31,y: 4 },
-    { x: 32,y: 4 },
-    { x: 20,y: 5 },
-    { x: 32,y: 5 },
-    { x: 20,y: 6 },
-    { x: 32,y: 6 },
-    { x: 20,y: 7 },
-    { x: 32,y: 7 },
-    { x: 20,y: 8 },
-    { x: 32,y: 8 },
-    { x: 20,y: 9 },
-    { x: 32,y: 9 },
-    { x: 20,y: 10 },
-    { x: 32,y: 10 },
-    { x: 20,y: 11 },
-    { x: 32,y: 11 },
-    { x: 20,y: 12 },
-    { x: 32,y: 12 },
-    { x: 20,y: 13 },
-    { x: 32,y: 13 },
-    { x: 20,y: 14 },
-    { x: 32,y: 14 },
-    { x: 20,y: 15 },
-    { x: 21,y: 15 },
-    { x: 22,y: 15 },
-    { x: 23,y: 15 },
-    { x: 24,y: 15 },
-    { x: 25,y: 15 },
-    { x: 26,y: 15 },
-    { x: 27,y: 15 },
-    { x: 28,y: 15 },
-    { x: 29,y: 15 },
-    { x: 30,y: 15 },
-    { x: 31,y: 15 },
-    { x: 32,y: 15 },
+    { x: 29, y: 7, status: "teleport" },
+    { x: 4, y: 15, status: "teleport" },
+    { x: 20, y: 4 },
+    { x: 21, y: 4 },
+    { x: 22, y: 4 },
+    { x: 23, y: 4 },
+    { x: 24, y: 4 },
+    { x: 25, y: 4 },
+    { x: 26, y: 4 },
+    { x: 27, y: 4 },
+    { x: 28, y: 4 },
+    { x: 29, y: 4 },
+    { x: 30, y: 4 },
+    { x: 31, y: 4 },
+    { x: 32, y: 4 },
+    { x: 20, y: 5 },
+    { x: 32, y: 5 },
+    { x: 20, y: 6 },
+    { x: 32, y: 6 },
+    { x: 20, y: 7 },
+    { x: 32, y: 7 },
+    { x: 20, y: 8 },
+    { x: 32, y: 8 },
+    { x: 20, y: 9 },
+    { x: 32, y: 9 },
+    { x: 20, y: 10 },
+    { x: 32, y: 10 },
+    { x: 20, y: 11 },
+    { x: 32, y: 11 },
+    { x: 20, y: 12 },
+    { x: 32, y: 12 },
+    { x: 20, y: 13 },
+    { x: 32, y: 13 },
+    { x: 20, y: 14 },
+    { x: 32, y: 14 },
+    { x: 20, y: 15 },
+    { x: 21, y: 15 },
+    { x: 22, y: 15 },
+    { x: 23, y: 15 },
+    { x: 24, y: 15 },
+    { x: 25, y: 15 },
+    { x: 26, y: 15 },
+    { x: 27, y: 15 },
+    { x: 28, y: 15 },
+    { x: 29, y: 15 },
+    { x: 30, y: 15 },
+    { x: 31, y: 15 },
+    { x: 32, y: 15 },
   ],
   level2: [
-    { x: 1,y: 1,status: "wall" },
-    { x: 2,y: 1,status: "wall" },
-    { x: 1,y: 2,status: "wall" },
-    { x: 2,y: 2,status: "wall" },
-    { x: 38,y: 18,status: "wall" },
-    { x: 37,y: 18,status: "wall" },
-    { x: 38,y: 17,status: "wall" },
-    { x: 37,y: 17,status: "wall" },
+    { x: 1, y: 1, status: "wall" },
+    { x: 2, y: 1, status: "wall" },
+    { x: 1, y: 2, status: "wall" },
+    { x: 2, y: 2, status: "wall" },
+    { x: 38, y: 18, status: "wall" },
+    { x: 37, y: 18, status: "wall" },
+    { x: 38, y: 17, status: "wall" },
+    { x: 37, y: 17, status: "wall" },
   ],
 };
 
 const APPLE = {
   x: Math.floor(Math.random() * (gw / CELL)),
   y: Math.floor(Math.random() * (gh / CELL)),
-  radius: 10
+  radius: 10,
 };
 
+const areaColor = document.querySelector("#area-color");
+const colorSnakeBody = document.querySelector("#snake-body-color");
+const colorSnakeHead = document.querySelector("#snake-head-color");
+const colorApple = document.querySelector("#apple-color");
+const colorWall = document.querySelector("#wall-color");
+const colorTeleport = document.querySelector("#teleport-color");
 
+areaColor.addEventListener("input", () => (cnv.style.backgroundColor = areaColor.value));
+colorSnakeBody.addEventListener("input", () => (COLORS.snakeBody = colorSnakeBody.value));
+colorSnakeHead.addEventListener("input", () => (COLORS.snakeHead = colorSnakeHead.value));
+colorApple.addEventListener("input", () => (COLORS.apple = colorApple.value));
+colorWall.addEventListener("input", () => (COLORS.wall = colorWall.value));
+colorTeleport.addEventListener("input", () => (COLORS.teleport = colorTeleport.value));
 
-const areaColor = document.querySelector('#area-color')
-const colorSnakeBody = document.querySelector('#snake-body-color')
-const colorSnakeHead = document.querySelector('#snake-head-color')
-const colorApple = document.querySelector('#apple-color')
-const colorWall = document.querySelector('#wall-color')
-const colorTeleport = document.querySelector('#teleport-color')
-
-areaColor.addEventListener('input',() => cnv.style.backgroundColor = areaColor.value)
-colorSnakeBody.addEventListener('input',() => COLORS.snakeBody = colorSnakeBody.value)
-colorSnakeHead.addEventListener('input',() => COLORS.snakeHead = colorSnakeHead.value)
-colorApple.addEventListener('input',() => COLORS.apple = colorApple.value)
-colorWall.addEventListener('input',() => COLORS.wall = colorWall.value)
-colorTeleport.addEventListener('input',() => COLORS.teleport = colorTeleport.value)
-
-let colorsPanelOpen = false
-colorsPanelBtn.addEventListener('click',() => {
+let colorsPanelOpen = false;
+colorsPanelBtn.addEventListener("click", () => {
   if (!colorsPanelOpen) {
-    colorsPanel.classList.add('colors-control-open')
-    colorsPanelOpen = true
+    colorsPanel.classList.add("colors-control-open");
+    colorsPanelOpen = true;
   } else {
-    colorsPanel.classList.remove('colors-control-open')
-    colorsPanelOpen = false
+    colorsPanel.classList.remove("colors-control-open");
+    colorsPanelOpen = false;
   }
-})
+});
--- level-gen.js
+++ level-gen.js
@@ -1,3 +1,7 @@
+chechSavesLevels();
+
+if (localStorage.getItem("save-levels") !== null) deleteAllLevelsBtn.disabled = false;
+
 let tempTeleport = [];
 
 function genLevel(Cube) {
@@ -11,48 +15,49 @@ function genLevel(Cube) {
       if (x === 5 && y === 3) div.classList.add("snake-gen-point");
       if (x === 4 && y === 3) div.classList.add("snake-gen-point");
 
-      div.setAttribute("data-x",x);
-      div.setAttribute("data-y",y);
+      div.setAttribute("data-x", x);
+      div.setAttribute("data-y", y);
       area.append(div);
     }
   }
 }
-genLevel(CELL)
+genLevel(CELL);
 
 let keyPress = false;
 let tempLevel = [];
 let tempLevelSave = {};
+let allLocalSaveLevels = [];
 
-document.addEventListener("keydown",(e) => {
+document.addEventListener("keydown", (e) => {
   if (e.keyCode === 17) keyPress = true;
 });
 
-document.addEventListener("keyup",(e) => {
+document.addEventListener("keyup", (e) => {
   if (e.keyCode === 17) keyPress = false;
 });
 
-document.addEventListener("mousedown",(e) => {
+document.addEventListener("mousedown", (e) => {
   //e.preventDefault();
   keyPress = true;
 });
 
-document.addEventListener("mouseup",(e) => {
+document.addEventListener("mouseup", (e) => {
   keyPress = false;
 });
 
-area.addEventListener("mouseover",(e) => {
+area.addEventListener("mouseover", (e) => {
   if (e.target.classList.contains("box") && keyPress) {
     mouseOverBox(e);
   }
 });
-area.addEventListener("click",(e) => {
+area.addEventListener("click", (e) => {
   if (e.target.classList.contains("box")) {
     mouseOverBox(e);
   }
 });
 
-genBtn.addEventListener("click",generate);
-rstBtn.addEventListener("click",resetGenerator);
+genBtn.addEventListener("click", generate);
+rstBtn.addEventListener("click", resetGenerator);
 
 function mouseOverBox(e) {
   if (e.target.classList.contains("snake-gen-point")) return;
@@ -113,8 +118,8 @@ function generate() {
   resetGame();
   if (tempLevel.length === 0) {
     tempLevel = [
-      { x: -1,y: -1 },
-      { x: -2,y: -2 },
+      { x: -1, y: -1 },
+      { x: -2, y: -2 },
     ];
   }
   MAPS.level1 = tempLevel;
@@ -137,31 +142,31 @@ function resetGenerator() {
 }
 
 function bigWorldOn() {
-  area.innerHTML = ''
-  CELL = 10
-  APPLE.radius = CELL / 2
+  area.innerHTML = "";
+  CELL = 10;
+  APPLE.radius = CELL / 2;
 
-  genLevel(CELL)
+  genLevel(CELL);
 
   area.style.gridTemplateColumns = "repeat(80, 10px)";
-  document.querySelectorAll('.box').forEach(el => {
+  document.querySelectorAll(".box").forEach((el) => {
     el.style.width = "10px";
     el.style.height = "10px";
-  })
+  });
 }
 
 function bigWorldOff() {
-  area.innerHTML = ''
-  CELL = 20
-  APPLE.radius = CELL / 2
+  area.innerHTML = "";
+  CELL = 20;
+  APPLE.radius = CELL / 2;
 
-  genLevel(CELL)
+  genLevel(CELL);
 
   area.style.gridTemplateColumns = "repeat(40, 20px)";
-  document.querySelectorAll('.box').forEach(el => {
+  document.querySelectorAll(".box").forEach((el) => {
     el.style.width = "20px";
     el.style.height = "20px";
-  })
+  });
 }
 
 function saveLevelStart() {
@@ -188,54 +193,180 @@ function saveLevelStart() {
 
   if (tempLevel.length === 0) {
     tempLevel = [
-      { x: -1,y: -1 },
-      { x: -2,y: -2 },
+      { x: -1, y: -1 },
+      { x: -2, y: -2 },
     ];
   }
 
-  saveLevelScreen.style.display = 'flex'
+  saveLevelScreen.style.display = "flex";
 
-
-
-
-  let jsonLevel = JSON.stringify(tempLevel)
-
-  localStorage.setItem('Level-Name',jsonLevel)
-  console.log(tempLevel)
-
-  saveLevelsOut.innerHTML = 'jsonLevel'
-
-
-  tempLevel = []
+  document.querySelector("#save-level-name-input").addEventListener("input", () => {
+    document.querySelector("#save-level-name-input").value !== "" ? (document.querySelector("#save-level-name-btn").disabled = false) : (document.querySelector("#save-level-name-btn").disabled = true);
+  });
 }
-
-
-
 
 function saveLevelEnd() {
-  let levelName = document.querySelector('#save-level-name-input').value
+  allLocalSaveLevels = [];
+  let levelName = document.querySelector("#save-level-name-input").value;
   tempLevelSave = {
-    Name: levelName,
-    Level: tempLevel
+    name: levelName,
+    level: tempLevel,
+  };
+
+  if (localStorage.getItem("save-levels") !== null) {
+    let savedLevels = JSON.parse(localStorage.getItem("save-levels"));
+    savedLevels.forEach((el) => {
+      allLocalSaveLevels.push(el);
+    });
   }
 
+  allLocalSaveLevels.push(tempLevelSave);
 
-  saveLevelScreen.style.display = 'flex'
+  saveLevelScreen.style.display = "none";
+
+  saveLevelsOut.innerHTML = "";
+
+  allLocalSaveLevels.forEach((el) => {
+    let span = document.createElement("span");
+    saveLevelsOut.appendChild(span);
+    span.classList.add("saved-level");
+    span.innerHTML = el.name;
+  });
+
+  exportInLocalStorage(allLocalSaveLevels);
+  tempLevel = [];
+
+  deleteAllLevelsBtn.disabled = false;
 }
 
-
-
-
-
-document.querySelector('#save-level-name-btn').onclick = () => {
-  console.log(levelName)
-  saveLevelScreen.style.display = 'none'
+function chechSavesLevels() {
+  if (localStorage.getItem("save-levels") === null) {
+    return;
+  } else {
+    let savedLevels = JSON.parse(localStorage.getItem("save-levels"));
+    savedLevels.forEach((el) => {
+      saveLevelsOut.innerHTML += `
+      <span class="saved-level">${el.name}</span>
+      `;
+    });
+  }
 }
 
-saveLevelBtn.addEventListener('click',saveLevelStart)
+function exportInLocalStorage(obj) {
+  localStorage.setItem("save-levels", JSON.stringify(obj));
+}
 
-bigWorldCheck.addEventListener('input',() => bigWorldCheck.checked ? bigWorldOn() : bigWorldOff())
+function importInLocalStorage() {
+  JSON.parse(localStorage.getItem("Save Levels"));
+}
+
+function loadLevelClick(e) {
+  if (e.target.classList.contains("saved-level")) {
+    let allLevels = JSON.parse(localStorage.getItem("save-levels"));
+    let boxes = area.querySelectorAll(".box");
+
+    boxes.forEach((el) => {
+      el.classList.remove("teleport");
+      el.classList.remove("black");
+    });
+
+    tempLoadLevel = [];
+    allLevels.forEach((el, i) => {
+      if (e.target.innerHTML === el.name) {
+        tempLoadLevel = el.level;
+      }
+    });
+
+    tempLoadLevel.forEach((el) => {
+      for (let b = 0; b < boxes.length; b++) {
+        if (+boxes[b].getAttribute("data-x") === +el.x && +boxes[b].getAttribute("data-y") === +el.y && el.status === "teleport") {
+          boxes[b].classList.add("teleport");
+        } else if (+boxes[b].getAttribute("data-x") === +el.x && +boxes[b].getAttribute("data-y") === +el.y) {
+          boxes[b].classList.add("black");
+        }
+      }
+    });
+  }
+}
+
+function deleteAllSavedLevels() {
+  if (localStorage.getItem("save-levels") !== null) {
+    localStorage.removeItem("save-levels");
+    saveLevelsOut.innerHTML = "";
+    allLocalSaveLevels = [];
+  } else {
+    return;
+  }
+  deleteAllLevelsBtn.disabled = true;
+  deleteSelectedLevelsBtn.disabled = true;
+}
+
+function deleteSelectedLevels() {
+  let selectedLevelsArr = [];
+  let selectedLevels = saveLevelsOut.querySelectorAll(".saved-level-checked");
+  selectedLevels.forEach((el) => selectedLevelsArr.push(el.innerHTML));
+
+  let localStorageLevels = JSON.parse(localStorage.getItem("save-levels"));
+  console.log(selectedLevelsArr);
+  console.log(localStorageLevels);
+
+  localStorageLevels.forEach((localName, i) => {
+    selectedLevelsArr.forEach((innerName) => {
+      if (localName.name === innerName) {
+        localStorageLevels.splice(i, 1);
+      }
+    });
+  });
+
+  localStorage.removeItem("save-levels");
+  localStorage.setItem("save-levels", JSON.stringify(localStorageLevels));
+
+  saveLevelsOut.innerHTML = "";
+
+  chechSavesLevels();
+
+  if (saveLevelsOut.innerHTML === "") {
+    localStorage.removeItem("save-levels");
+    deleteAllLevelsBtn.disabled = true;
+  }
+
+  deleteSelectedLevelsBtn.disabled = true;
+
+  //console.log(localStorageLevels);
+}
+
+function selectedLevels(e) {
+  if (e.target.classList.contains("saved-level")) {
+    e.preventDefault();
+    if (!e.target.classList.contains("saved-level-checked")) {
+      e.target.classList.add("saved-level-checked");
+    } else {
+      e.target.classList.remove("saved-level-checked");
+    }
+    let selectedLevelsArr = [];
+    let selectedLevels = saveLevelsOut.querySelectorAll(".saved-level-checked");
+    selectedLevels.forEach((el) => selectedLevelsArr.push(el.innerHTML));
+    if (selectedLevelsArr.length === 0) {
+      deleteSelectedLevelsBtn.disabled = true;
+    } else {
+      deleteSelectedLevelsBtn.disabled = false;
+    }
+  }
+}
+
+saveLevelsOut.addEventListener("contextmenu", selectedLevels);
+saveLevelsOut.addEventListener("click", loadLevelClick);
+deleteSelectedLevelsBtn.addEventListener("click", deleteSelectedLevels);
+deleteAllLevelsBtn.addEventListener("click", deleteAllSavedLevels);
+
+document.querySelector("#save-level-name-btn").onclick = () => {
+  saveLevelEnd();
+};
+
+saveLevelBtn.addEventListener("click", saveLevelStart);
+
+bigWorldCheck.addEventListener("input", () => (bigWorldCheck.checked ? bigWorldOn() : bigWorldOff()));
 
 // Integration
 
-document.querySelector(".generator-close").addEventListener("click",() => document.querySelector(".generator").style.display = "none");
+document.querySelector(".generator-close").addEventListener("click", () => (document.querySelector(".generator").style.display = "none"));
